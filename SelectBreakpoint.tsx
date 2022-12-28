249d6dd0d68991c099f0ddf3ce297c6cee79f843
Matthias Giger <i@matthiasgiger.com>
2022-12-28 18:53:13 +0100

    feat(breakpoint): make SelectBreakpoint responsive and generic
    release-npm

--- SelectBreakpoint.tsx
+++ SelectBreakpoint.tsx
@@ -1,33 +1,33 @@
 import React, { useEffect, useRef } from 'react'
-import { View, TouchableOpacity, Text, StyleSheet, Animated, Easing, ViewProps } from 'react-native'
-import { useResponsive, getBreakpoints } from './index'
+import { View, TouchableOpacity, Text, Animated, Easing, ViewProps } from 'react-native'
+import { useResponsive, getBreakpoints, Styled, getValue } from './index'
 
-const styles = StyleSheet.create({
-  wrapper: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  item: {
-    marginLeft: 20,
-    alignItems: 'center',
-    width: 60,
-  },
-  selected: {
-    position: 'absolute',
-    backgroundColor: 'red',
-    width: 70,
-    height: '100%',
-    borderRadius: 10,
-  },
+const Wrapper = Styled(View, {
+  flexDirection: 'row',
+  alignItems: 'center',
+  marginHorizontal: { small: 20 },
 })
 
-const getPosition = (index: number) => (index + 1) * 20 + index * 60 - 5
+const Button = Styled(TouchableOpacity, {
+  marginHorizontal: 20,
+  alignItems: 'center',
+  flex: 1,
+})
+
+const Cursor = Styled(Animated.View, {
+  position: 'absolute',
+  width: '33.3%',
+  height: '100%',
+  borderRadius: 10,
+})
+
+const getPosition = (index: number, length: number) => (100 / length) * index // (index + 1) * 20 + index * 60 - 5
 const getFontSize = (index: number) => 12 + index * 2
 const capitalizeFirstLetter = (value: string) => value.charAt(0).toUpperCase() + value.slice(1)
 
 const textStyle = (active: boolean, index: number) => {
   return {
-    fontSize: getFontSize(index),
+    fontSize: getValue(getFontSize(index)),
     color: active ? '#FFFFFF' : '#000000',
   }
 }
@@ -43,16 +43,26 @@ const animate = (handle: Animated.Value, toValue: number) =>
 type Props = {
   onChange?: (value: string) => void
   waitForAnimation?: boolean
+  color?: string
 } & ViewProps
 
-export const SelectBreakpoint = ({ onChange, waitForAnimation = false, ...props }: Props) => {
+export const SelectBreakpoint = ({
+  onChange,
+  waitForAnimation = false,
+  color = 'red',
+  ...props
+}: Props) => {
   const breakpoints = getBreakpoints()
   const { breakpoint, setBreakpoint } = useResponsive()
   const currentIndex = Object.keys(breakpoints).findIndex(
     (current) => current === String(breakpoint)
   )
-  const currentPosition = getPosition(currentIndex)
+  const currentPosition = getPosition(currentIndex, Object.keys(breakpoints).length)
   const position = useRef(new Animated.Value(currentPosition)).current
+  const animatedPosition = position.interpolate({
+    inputRange: [0, 100],
+    outputRange: ['0%', '100%'],
+  })
 
   useEffect(() => {
     // Trigger animation if breakpoint is changed from outside.
@@ -63,14 +73,13 @@ export const SelectBreakpoint = ({ onChange, waitForAnimation = false, ...props 
   }, [breakpoint, currentPosition, position])
 
   return (
-    <View style={styles.wrapper} {...props}>
-      <Animated.View style={[styles.selected, { left: position }]} />
+    <Wrapper {...props}>
+      <Cursor style={[{ backgroundColor: color }, { left: animatedPosition }]} />
       {Object.keys(breakpoints).map((key, index) => (
-        <TouchableOpacity
+        <Button
           key={key}
-          style={styles.item}
           onPress={() => {
-            animate(position, getPosition(index))
+            animate(position, getPosition(index, Object.keys(breakpoints).length))
 
             setTimeout(
               () => {
@@ -85,8 +94,8 @@ export const SelectBreakpoint = ({ onChange, waitForAnimation = false, ...props 
           }}
         >
           <Text style={textStyle(key === breakpoint, index)}>{capitalizeFirstLetter(key)}</Text>
-        </TouchableOpacity>
+        </Button>
       ))}
-    </View>
+    </Wrapper>
   )
 }
